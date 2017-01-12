693332800f98f0a3e7400323a095e8a32c940f64
Mike Heavers <mheavers@gmail.com>
2017-01-12 09:20:46 -0800

    implement data as ajax

--- src/App.js
+++ src/App.js
@@ -1,116 +1,19 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
 import Carousel from 'nuka-carousel'; //http://kenwheeler.github.io/nuka-carousel/#/
 
 //DATA
 
-const appData = {
-  address: {
-    street: '28 Summit',
-    city: 'Philmont, NY',
-    zip: '12565'
-  },
-  menuItems: [
-    { name: 'Gallery'},
-    { name: 'Details'},
-    { name: 'Virtual Tour', mobile: false},
-    { name: 'Map'},
-    { name: 'Contact'}
-  ],
 
-  carouselItems: [
-    { name: '1', image: '/images/carousel/bedroom.jpg'},
-    { name: '2', image: '/images/carousel/loft.jpg'},
-    { name: '3', image: '/images/carousel/swing.jpg'},
-    { name: '4', image: '/images/carousel/shed-yard.jpg'},
-    { name: '5', image: '/images/carousel/firepit.jpg'},
-    { name: '6', image: '/images/carousel/floorplans.jpg'}
-  ],
-
-  copy: {
-    title: '28 Summit',
-    subtitle: 'Modern living with upstate lakeside charm',
-    details: {
-      title: 'Home Details',
-      price: '179,000',
-      sections: [
-        { name: 'the main house',
-          details: [
-            '2 bedroom, 2 bath',
-            '1450 sq. ft of finished space',
-            'Single car garage with washer dryer',
-            'Huge lofted second level could accommodate extra bedroom',
-            'Radiant Heat, Wood Floors, & Wood Stove',
-            'Cathedral ceilings & abundant skylights',
-            'Warming room and upstairs rear deck'
-          ]
-        },
-        { name: 'the guest shed',
-          details: [
-            'Day bed & loft bed',
-            'Plenty of windows',
-            'Heated, private outdoor shower',
-            'Deck and fire pit'
-          ]
-        },
-        { name: 'the grounds',
-          details: [
-            '13,000 sq. ft (~.29 acres)',
-            'Shady fenced in back yard for pets & kids',
-            'Picnic area and private side yard',
-            'Privacy fences and shrubs around entire lot',
-            'Vegetable garden',
-            'Views of Summit Reservoir and Summit Mill'
-          ]
-        },
-        { name: 'the area',
-          details: [
-            'Just above High Falls (swimming & hiking)',
-            '~2 hours to NYC via train or car',
-            '5 min. to Hawthorne Valley Waldorf School & Farm Store',
-            '15 min. from charming towns like Hudson, Chatham & Hillsdale',
-            '20 min. to Catamount ski area or the Hudson River'
-          ]
-        }
-      ]
-    },
-    tour: {
-      title: 'Virtual Tour',
-      description: 'click the model below to begin interaction.',
-      model: 'https://3dwarehouse.sketchup.com/embed.html?mid=b49d60e2-08f9-4240-8e3d-83d3e245f915&width=1200&height=480',
-      scene: 'https://3dwarehouse.sketchup.com/embed.html?mid=c25e3b04-8de0-4743-9bb0-9b7940b8f59a&width=1200&height=480'
-    },
-    contact: {
-      title: 'Contact',
-      description: 'We would love to show you the house. Get in touch with us in order to schedule a viewing or learn about the next open house.',
-      email: 'palomacmedina@gmail.com',
-      phone: '503-453-0172',
-      tel: '+15034530172'
-    }
-  },
-  maps: {
-    title: 'Maps',
-    descripion: 'Select the map you wish to view',
-    maps: [
-      {
-        name: 'Area',
-        link: 'https://www.google.com/maps/d/embed?mid=1fNl1qw1RtYJvYz63D7LKWv8LvAM'
-      },
-      {
-        name: 'Town',
-        link: 'https://www.google.com/maps/d/embed?mid=1qoBsE-hl8BAg56bqDCU-ftX2MsM'
-      }
-    ]
-  }
-}
 
 //HEADER
 
 function AppHeader(props) {
   return (
     <div className="App-header">
-      <h2 className="App-title">{appData.copy.title}</h2>
-      <h4 className="App-subtitle">{appData.copy.subtitle}</h4>
+      <h2 className="App-title">{props.appData.copy.title}</h2>
+      <h4 className="App-subtitle">{props.appData.copy.subtitle}</h4>
     </div>
   );
 }
@@ -124,7 +27,7 @@ function AppMenuItem(props){
 
 function AppMenu(props) {
 
-  const menuItems = appData.menuItems.map((menuItem) =>
+  const menuItems = props.appData.menuItems.map((menuItem) =>
     <AppMenuItem key={menuItem.name.toLowerCase()} name={menuItem.name} mobile={menuItem.mobile === false ? ' no-mobile' : ''} />
   );
 
@@ -147,8 +50,12 @@ function AppCarouselItem(props){
 class MainCarousel extends Component {
   mixins: [Carousel.ControllerMixin];
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    const carouselItems = appData.carouselItems.map((carouselItem) =>
+    const carouselItems = this.props.appData.carouselItems.map((carouselItem) =>
       <AppCarouselItem key={carouselItem.name.toLowerCase()} name={carouselItem.name} image={carouselItem.image} />
     );
     return (
@@ -180,17 +87,17 @@ function HomeDetailSection(props){
 }
 
 function HomeDetails(props){
-  const homeDetailSections = appData.copy.details.sections.map((detailSection,i) =>
+  const homeDetailSections = props.appData.copy.details.sections.map((detailSection,i) =>
     <HomeDetailSection key={detailSection.name.toLowerCase()} name={detailSection.name} details={detailSection.details} col={i%2 ? 'even' : 'odd'}/>
   );
 
   return(
     <div className="App-details">
-      <h3 className="details-title">{appData.copy.details.title}</h3>
+      <h3 className="details-title">{props.appData.copy.details.title}</h3>
       <div className="details-sections clearfix">
         {homeDetailSections}
       </div>
-      <h4 className="details-price">Asking: ${appData.copy.details.price}</h4>
+      <h4 className="details-price">Asking: ${props.appData.copy.details.price}</h4>
     </div>
   );
 }
@@ -199,21 +106,17 @@ function HomeDetails(props){
 
 class Model3D extends React.Component{
 
-  componentDidMount() {
-    /*console.log(this.modelframe); //MH - attempt to auto-initiate model
-    setTimeout(()=>{
-      console.log('click');
-      this.modelframe.click()
-    },5000);*/
+  constructor(props) {
+    super(props);
   }
 
-  render(){
+  render(props){
     return(
       <div className="model-outer no-mobile">
-        <h3 className="details-title">{appData.copy.tour.title}</h3>
-        <p className="tour-description">{appData.copy.tour.description}</p>
+        <h3 className="details-title">{this.props.appData.copy.tour.title}</h3>
+        <p className="tour-description">{this.props.appData.copy.tour.description}</p>
         <div className="model-container">
-          <iframe ref={(iframe) => { this.modelframe = iframe; }} className="model-frame" src={appData.copy.tour.model} allowFullScreen></iframe>
+          <iframe ref={(iframe) => { this.modelframe = iframe; }} className="model-frame" src={this.props.appData.copy.tour.model} allowFullScreen></iframe>
         </div>
       </div>
     )
@@ -235,13 +138,13 @@ function AppMapSection(props){
 }
 
 function AppMaps(props){
-  const appMapSections = appData.maps.maps.map((mapItem,i) =>
+  const appMapSections = props.appData.maps.maps.map((mapItem,i) =>
     <AppMapSection key={mapItem.name.toLowerCase()} name={mapItem.name} link={mapItem.link} col={i%2 ? 'even' : 'odd'}/>
   );
 
   return(
     <div className="App-maps">
-      <h3 className="details-title">{appData.maps.title}</h3>
+      <h3 className="details-title">{props.appData.maps.title}</h3>
       <div className="map-sections">
         {appMapSections}
       </div>
@@ -254,11 +157,11 @@ function AppMaps(props){
 function AppContact(props){
   return(
     <div className="App-contact">
-      <h3 className="details-title">{appData.copy.contact.title}</h3>
+      <h3 className="details-title">{props.appData.copy.contact.title}</h3>
       <div className="app-contact-container">
-        <p className="contact-description">{appData.copy.contact.description}</p>
-        <p className="contact-method"><span className="oswald">Phone:</span><br/> <a href={"tel:" + appData.copy.contact.tel}>{appData.copy.contact.phone}</a></p>
-        <p className="contact-method"><span className="oswald">Email:</span><br/><a href={"mailto:" + appData.copy.contact.email}>{appData.copy.contact.email}</a></p>
+        <p className="contact-description">{props.appData.copy.contact.description}</p>
+        <p className="contact-method"><span className="oswald">Phone:</span><br/> <a href={"tel:" + props.appData.copy.contact.tel}>{props.appData.copy.contact.phone}</a></p>
+        <p className="contact-method"><span className="oswald">Email:</span><br/><a href={"mailto:" + props.appData.copy.contact.email}>{props.appData.copy.contact.email}</a></p>
       </div>
     </div>
   )
@@ -268,18 +171,46 @@ function AppContact(props){
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {appData: {}};
+  }
+
+  componentDidMount() {
+    axios.get('/data/appData.json')
+    .then((result)=> {
+      const thisData = result.data;
+      this.setState({
+        appData: result.data
+      });
+    })
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <AppHeader />
-        <AppMenu />
-        <MainCarousel/>
-        <HomeDetails/>
-        <Model3D />
-        <AppMaps />
-        <AppContact />
-      </div>
-    );
+
+    const theData = this.state.appData;
+    if (Object.keys(theData).length > 0 && theData.constructor === Object){ //if the object is not empty
+      return (
+        <div className="App">
+          <AppHeader appData={theData} />
+          <AppMenu appData={theData} />
+          <MainCarousel appData={theData} />
+          <HomeDetails appData={theData} />
+          <Model3D appData={theData} />
+          <AppMaps appData={theData} />
+          <AppContact appData={theData} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App"></div>
+      )
+    }
+
   }
 }
 
